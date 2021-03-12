20d09114bfa34134643a428b0b47ff52d54e0ca0
ziv <791421562@qq.com>
2021-03-12 03:28:06 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/','StaticPagesController@home');
-Route::get('/about','StaticPagesController@about');
-Route::get('/help','StaticPagesController@help');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/help','StaticPagesController@help')->name('help');
