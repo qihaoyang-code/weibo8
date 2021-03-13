e8e3f57b19f94cce9c24e5a6741642043cdbd694
ziv <791421562@qq.com>
2021-03-13 08:01:15 +0000

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Str;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -45,6 +45,14 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($user){
+            $user->activation_token = Str::random(10);
+        });
     }
 
 }
